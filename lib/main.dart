c69856959b9cb09c97c3eebbd33f12e70079c455
Dang Tran Khanh <57286986+khanhdk0000@users.noreply.github.com>
2020-06-30 17:31:00 +0700

    create log in

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ import './providers/filtered_saved_list.dart';
 import './screens/scan_screen.dart';
 import './screens/welcome_screen.dart';
 import './screens/navigate_screen.dart';
+import './screens/login/login_option_screen.dart';
+import './screens/login/Register_screen.dart';
 
 // The following are all the files supporting for Plan For A Diet.
 import './plan_for_a_diet/plan_for_a_diet_screens/health_data_input_screen.dart';
@@ -60,6 +62,8 @@ class _MyAppState extends State<MyApp> {
         home: WelcomePage(),
         //Setting route for pages here
         routes: {
+          'RegisterPage': (context) => RegisterPage(),
+          'LoginRegisterPage': (context) => LoginRegisterPage(),
           'NavigatePage': (context) => NavigatePage(),
           'WelcomePage': (context) => WelcomePage(),
           'HomePage': (context) => HomePage(),
--- lib/screens/navigate_screen.dart
+++ lib/screens/navigate_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 
 class NavigatePage extends StatelessWidget {
   @override
