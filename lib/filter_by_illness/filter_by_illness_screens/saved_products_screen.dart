bde95a6b4b66bd4f033225f4e56b9446e919d8fc
AagonP <54505544+AagonP@users.noreply.github.com>
2020-08-04 20:42:56 +0700

    Update buttons UI

--- lib/filter_by_illness/filter_by_illness_screens/saved_products_screen.dart
+++ lib/filter_by_illness/filter_by_illness_screens/saved_products_screen.dart
@@ -24,7 +24,6 @@ class _SavedProductsScreenState extends State<SavedProductsScreen>
     );
     final List<Product> _savedProducts =
         Provider.of<SavedProducts>(context).savedProducts.toSet().toList();
-    
 
     return Scaffold(
         appBar: AppBar(
@@ -32,77 +31,169 @@ class _SavedProductsScreenState extends State<SavedProductsScreen>
           title: Text('My saved products'),
         ),
         body: GridView.count(
-          shrinkWrap: true,
+          childAspectRatio: 0.85,
           crossAxisCount: 2,
           children: List.generate(
             _savedProducts.length,
             (index) {
-              return Card(
-                shadowColor: Colors.grey,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
+              // return Card(
+              //   shadowColor: Colors.grey,
+              //   shape: RoundedRectangleBorder(
+              //     borderRadius: BorderRadius.circular(20.0),
+              //   ),
+              //   margin: EdgeInsets.all(15),
+              //   child: Column(
+              //     crossAxisAlignment: CrossAxisAlignment.stretch,
+              //     children: <Widget>[
+              //       Container(
+              //         padding: EdgeInsets.all(5),
+              //         child: GestureDetector(
+              //           onTap: () {
+              //             navigateToFoodInfoScreen(
+              //                 index, context, _savedProducts);
+              //           },
+              //           // slide image function here
+              //           child: ClipRRect(
+              //             borderRadius: BorderRadius.circular(20.0),
+              //             child: getProductImage(index, _savedProducts),
+              //           ),
+              //         ),
+              //       ),
+              //       Container(
+              //         margin: EdgeInsets.symmetric(horizontal: 5),
+              //         width: 150,
+              //         height: 35,
+              //         child: AutoSizeText(
+              //           _savedProducts.elementAt(index).name,
+              //           style: TextStyle(fontSize: 15.0),
+              //           minFontSize: 10.0,
+              //           maxLines: 2,
+              //           overflow: TextOverflow.ellipsis,
+              //         ),
+              //       ),
+              //       Expanded(
+              //         child: Row(
+              //           mainAxisAlignment: MainAxisAlignment.end,
+              //           children: <Widget>[
+              //             Container(
+              //               width: 50,
+              //               height: 50,
+              //               margin: EdgeInsets.all(1),
+              //               child: Card(
+              //                 elevation: 5,
+              //                 shape: RoundedRectangleBorder(
+              //                     borderRadius: BorderRadius.circular(10.0)),
+              //                 color: Colors.white,
+              //                 child: Builder(
+              //                   builder: (context) => IconButton(
+              //                     iconSize: 15,
+              //                     onPressed: () {
+              //                       Scaffold.of(context)
+              //                           .showSnackBar(removeFromSavedProducts);
+              //                       removeUserSavedProduct(
+              //                           index, context, _savedProducts);
+              //                     },
+              //                     icon: Icon(Icons.delete),
+              //                   ),
+              //                 ),
+              //               ),
+              //             ),
+              //           ],
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // );
+              return Container(
                 margin: EdgeInsets.all(15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      child: GestureDetector(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 33,
+                        color: Color(0xFFD3D3D3).withOpacity(.84),
+                      ),
+                    ],
+                  ),
+                  margin: EdgeInsets.all(5),
+                  child: Column(
+                    children: <Widget>[
+                      GestureDetector(
                         onTap: () {
-                          navigateToFoodInfoScreen(
-                              index, context, _savedProducts);
+                          Navigator.pushNamed(context, 'FoodInfoScreen',
+                              arguments: _savedProducts.elementAt(index));
                         },
-                        // slide image function here
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20.0),
-                          child: getProductImage(index, _savedProducts),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: (_savedProducts.elementAt(index).photoURL !=
+                                    null)
+                                ? Image.network(
+                                    _savedProducts.elementAt(index).photoURL,
+                                  )
+                                : Container(
+                                    height: 90.0,
+                                    color: Colors.white,
+                                  ),
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 5),
-                      width: 150,
-                      height: 35,
-                      child: AutoSizeText(
-                        _savedProducts.elementAt(index).name,
-                        style: TextStyle(fontSize: 15.0),
-                        minFontSize: 10.0,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+                      Row(
                         children: <Widget>[
-                          Container(
-                            width: 50,
-                            height: 50,
-                            margin: EdgeInsets.all(1),
-                            child: Card(
-                              elevation: 5,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0)),
-                              color: Colors.white,
-                              child: Builder(
-                                builder: (context) => IconButton(
-                                  onPressed: () {
-                                    Scaffold.of(context)
-                                        .showSnackBar(removeFromSavedProducts);
-                                    removeUserSavedProduct(
-                                        index, context, _savedProducts);
-                                  },
-                                  icon: Icon(Icons.delete),
-                                  iconSize: 25,
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 0, horizontal: 5),
+                              child: AutoSizeText(
+                                _savedProducts.elementAt(index).name,
+                                style: TextStyle(fontSize: 15.0),
+                                minFontSize: 10.0,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Expanded(child: Container()),
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () async {
+                                //Function goes here,
+                                Scaffold.of(context)
+                                    .showSnackBar(removeFromSavedProducts);
+                                removeUserSavedProduct(
+                                    index, context, _savedProducts);
+                              },
+                              child: Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF393939),
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20.0),
+                                    bottomRight: Radius.circular(20.0),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Del',
+                                  style: TextStyle(color: Colors.white),
                                 ),
                               ),
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             },
